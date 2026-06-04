3aa679f790eb483fbf601d43bd2c57140cb192b4
Sumanth <sumanthkumarmondu@gmail.com>
2026-06-04 21:14:25 +0530

    fixed AES-128 keyExpansion and addRoundKey logic

--- rtl/addRoundKey.sv
+++ rtl/addRoundKey.sv
@@ -11,6 +11,7 @@ module addRoundKey(
     input state_matrix_t state,  // input state matrix
     input logic [255:0] master_key,  // input master KEY
     input logic [112:0] rand_num,  // random bits from TRNG for sbox
+    input unibble round_num,  // input CIPHER which indicates number of AES rounds
     input logic trng_dead_flag,  // input to sbox from TRNG to indicate that TRNG has some fatal error
     input logic [1:0] key_size,  // input from CONTROL register specifying the KEY size
     input rst_n, clk);
@@ -31,30 +32,28 @@ module addRoundKey(
         32'h3600_0000   // Index 10 (Round 10)
     };
 
-    unibble round_cntr;  // keeps track of round number
-    logic [3:0][5:0] idx;  // keeps track of word number
     word_t subByte, sbox_state;  // input and outputs of sbox
-    state_matrix_t expKey;  // expanded KEYs by KeyExpansion logic
+    expKey_matrix_t expKey;  // expanded KEYs by KeyExpansion logic
+    expKey_matrix_t prev_expKey;  // these are previous round KEYs which are used in current round 
     logic sbox_done;  // addRoundKey can know when Sbox is done with computing necessary subBytes
-    logic sbox_done_delayed;  // delayed version of sbox_done for addRoundKeyOut purpose
 
     // sbox that gives subBytes used when i % Nk = 0 in KeyExpansion
     sbox#(32) Sbox(subByte, sbox_done, rst_trng, trng_dead_flag, sbox_state, rand_num, rst_n, clk);
 
     // optimized modulus function, based on key_size corresponding modulus is performed
-    // key_size = 00 - 128-bit KEY - so mod 4 is performed
-    // key_size = 01 - 192-bit KEY - so mod 6 is performed
-    // key_size = 10 - 256-bit KEY - so mod 8 and mod of 8 = 4 is performed
+    // key_size = 01 - 128-bit KEY - so mod 4 is performed
+    // key_size = 10 - 192-bit KEY - so mod 6 is performed
+    // key_size = 11 - 256-bit KEY - so mod 8 and mod of 8 = 4 is performed
     function automatic bit mod_Nk(input logic [5:0] idx, input logic [1:0] keySize);
         logic [1:0] sum_even_pos, sum_odd_pos;
         bit is_div_by_3;
 
         case(keySize)
-            2'b00, 2'b10: begin  // checks if idx % 4 = 0, idx % 8 = 0 and idx % 8 = 4
+            2'b01, 2'b11: begin  // checks if idx % 4 = 0, idx % 8 = 0 and idx % 8 = 4
                 if(idx[1:0] == 2'b00) return 1;
                 else return 0;
             end
-            2'b01: begin  // checks if idx % 6 = 0
+            2'b10: begin  // checks if idx % 6 = 0
                 // since divisible by 2, proceeding to check if divisible by 3
                 if(!idx[0]) begin 
                     sum_even_pos = {(idx[4] & idx[2]), (idx[4] ^ idx[2])};  // half adder logic
@@ -93,25 +92,9 @@ module addRoundKey(
         return div_res;
     endfunction
 
-    // optimized (num + 1) function
-    // this function works on the idea: from lsb if there 'n' no. of consecutive numbers then 
-    // any 1's from msb side remain in their position while remaining bits would become (1 << n)
-    // in simple words, bit flips if and only if all lower-ranking bits are high
-    // this function is used in keyExpansion to calculate corresponding word indeces based on round count
-    function automatic unibble add_1(input unibble num);
-        unibble result;
-
-        result[0] = ~num[0];
-        result[1] = num[1] ^ num[0];
-        result[2] = num[2] ^ (num[1] & num[0]);
-        result[3] = num[3] ^ (num[2] & num[1] & num[0]);
-        return result;
-    endfunction
-
     // function to left rotate the bytes in a given word
     function automatic word_t rotWord(input word_t word);
         word_t rot_word;
-        
         rot_word[31:24] = word[7:0];
         rot_word[23:16] = word[31:24];
         rot_word[15:8] = word[23:16];
@@ -119,78 +102,73 @@ module addRoundKey(
         return rot_word;
     endfunction
 
+    // core logic of KeyExpansion()
+    always_comb begin
+        case(key_size)
+            2'b01: begin  // AES-128
+                if(round_num == 0) begin
+                    for(int i=0; i<4; i++) 
+                        {expKey[3][i], expKey[2][i], expKey[1][i], expKey[0][i]} = 32'h0000_0000;
+                end
+                else begin  // actual KeyExpansion starts from round=1
+                    for(int i=0; i<4; i++) begin 
+                        if(i == 0) begin  // since this index is multiple of 4 it satisfies i%Nk = 0. So special transformation is applied
+                            sbox_state = rotWord({prev_expKey[3][3], prev_expKey[2][3], prev_expKey[1][3], prev_expKey[0][3]});  // loading Sbox input
+                            {expKey[3][0], expKey[2][0], expKey[1][0], expKey[0][0]} = {prev_expKey[3][0], prev_expKey[2][0], prev_expKey[1][0], prev_expKey[0][0]} ^ subByte ^ RCON[div_Nk(6'(round_num << 2), 4)];
+                        end
+                        else  // remaining all indices don't satisfy i%Nk = 0. So normal transformation is applied
+                            {expKey[3][i], expKey[2][i], expKey[1][i], expKey[0][i]} = {prev_expKey[3][i], prev_expKey[2][i], prev_expKey[1][i], prev_expKey[0][i]} ^ {expKey[3][i-1], expKey[2][i-1], expKey[1][i-1], expKey[0][i-1]};
+                    end
+                end
+            end
+            2'b10: begin  // AES-192
+                
+            end
+            2'b11: begin  // AES-256
+                
+            end
+            default: 
+        endcase
+    end
+
     // core logic of addRoundKey()
     always_ff @(posedge clk) begin
         if(!rst_n) begin
-            round_cntr <= 4'h0;
-            sbox_state <= 0;
-            subByte <= 0;
-            sbox_done_delayed <= 0;
-            for(int i=0; i<16; i++) begin 
-                addRoundKeyOut[i/4][i%4] <= 8'h00;
-                expKey[i/4][i%4] <= 8'h00;  
-                idx[i/4][i%4] <= 0;
-            end
-
-            // selecting initial index of word based on KEY size
-            case(key_size)
-                2'b00: word_idx <= 4'h4;
-                2'b01: word_idx <= 4'h6;
-                2'b10: word_idx <= 4'h8; 
-                default: word_idx <= 4'h4;
-            endcase
+            for(int i=0; i<32; i++) prev_expKey[i/8][i%8] <= 8'h00;
+            for(int i=0; i<16; i++) addRoundKeyOut[i/4][i%4] <= 8'h00;
         end
         else begin
-            sbox_done_delayed <= sbox_done;  // delaying sbox_done by one cycle
-
             case(key_size)
-                2'b00: begin
-                    if(round_cntr == 0) begin
+                2'b01: begin  // AES-128
+                    if(round_num == 0) begin  // first round simply uses master KEY
                         for(int i=0; i<4; i++) begin
                             // simply loading maskter KEY into expKey for further usage
-                            {expKey[3][i], expKey[2][i], expKey[1][i], expKey[0][i]} <= master_key[(32*i) +: 32];
+                            {prev_expKey[3][i], prev_expKey[2][i], prev_expKey[1][i], prev_expKey[0][i]} <= master_key[(32*i) +: 32];
 
                             // adding round KEY for round-0
                             {addRoundKeyOut[3][i], addRoundKeyOut[2][i], addRoundKeyOut[1][i], addRoundKeyOut[0][i]} <= {state[3][i], state[2][i], state[1][i], state[0][i]} ^ master_key[(32*i) +: 32];
                         end
                     end
-                    else begin
-                        idx[0] <= {2'b00, (round_cntr << 2)};  // i = round * 4
-                        idx[1] <= {2'b00, add_1(round_cntr << 2)};  // i = (round * 4) + 1
-                        idx[2] <= {2'b00, add_1(add_1(round_cntr << 2))};  // i = (round * 4) + 2
-                        idx[3] <= {2'b00, add_1(add_1(add_1(round_cntr << 2)))};  // i = (round * 4) + 3
-
-                        for(int i=0; i<4; i++) begin  // Key Expansion logic
-                            if(i%2 == 0) begin  // since idx[0] and idx[2] are always even they might satisfy i % Nk = 0. so these indices need checking and based on that expanded KEYs are computed
-                                if(mod_Nk(idx[i], key_size)) begin  // since condition is met, special transformation is applied
-                                    sbox_state <= rotWord({expKey[3][idx[i]-1], expKey[2][idx[i]-1], expKey[1][idx[i]-1], expKey[0][idx[i]]-1});  // loading Sbox input
-
-                                    if(sbox_done)  // keyExpansion can be done now since Sbox has given required subBytes
-                                        {expKey[3][idx[i]], expKey[2][idx[i]], expKey[1][idx[i]], expKey[0][idx[i]]} <= {expKey[3][idx[i]-4], expKey[2][idx[i]-4], expKey[1][idx[i]-4], expKey[0][idx[i]-4]}
-                                                                                                                        ^ subByte ^ RCON[div_Nk(idx[i], 4)];
-                                    else  // Sbox still hasn't computed required subBytes
-                                        {expKey[3][idx[i]], expKey[2][idx[i]], expKey[1][idx[i]], expKey[0][idx[i]]} <= {expKey[3][idx[i]], expKey[2][idx[i]], expKey[1][idx[i]], expKey[0][idx[i]]};
-                                end
-                                else
-                                    {expKey[3][idx[i]], expKey[2][idx[i]], expKey[1][idx[i]], expKey[0][idx[i]]} <= {expKey[3][idx[i]-4], expKey[2][idx[i]-4], expKey[1][idx[i]-4], expKey[0][idx[i]-4]}
-                                                                                                                    ^ {expKey[3][idx[i]-1], expKey[2][idx[i]-1], expKey[1][idx[i]-1], expKey[0][idx[i]]-1};
+                    else begin  // remianing rounds use expanded KEYs
+                        for(int i=0; i<4; i++) begin
+                            if(sbox_done) begin// updating the register since sbox_done is high  
+                                // updating current round KEYs so that these can be used in next round as previous round KEYs
+                                {prev_expKey[3][i], prev_expKey[2][i], prev_expKey[1][i], prev_expKey[0][i]} <= {expKey[3][i], expKey[2][i], expKey[1][i], expKey[0][i]};
+                                {addRoundKeyOut[3][i], addRoundKeyOut[2][i], addRoundKeyOut[1][i], addRoundKeyOut[0][i]} <= {state[3][i], state[2][i], state[1][i], state[0][i]} ^ {expKey[3][i], expKey[2][i], expKey[1][i], expKey[0][i]};
                             end
-                            else  // since idx[1] and idx[3] are always odd, checking if(i % Nk = 0) for these indices is not needed. they always satisfy i % Nk != 0 since Nk is even (4)
-                                {expKey[3][idx[i]], expKey[2][idx[i]], expKey[1][idx[i]], expKey[0][idx[i]]} <= {expKey[3][idx[i]-4], expKey[2][idx[i]-4], expKey[1][idx[i]-4], expKey[0][idx[i]-4]}
-                                                                                                                ^ {expKey[3][idx[i]-1], expKey[2][idx[i]-1], expKey[1][idx[i]-1], expKey[0][idx[i]]-1};
-
-                            // among the 4 expanded keys, the key that's going to consume more cycles is the one which uses Sbox because Sbox takes 6 cycles to compute subBytes
-                            // so waiting until delayed version of sbox_done ensures that expKey actually got updated so addRoundKeyOut can use it
-                            for(int i=0; i<4; i++) begin  // Add Round Key logic
-                                if(sbox_done_delayed)  
-                                    {addRoundKeyOut[3][i], addRoundKeyOut[2][i], addRoundKeyOut[1][i], addRoundKeyOut[0][i]} <= {state[3][i], state[2][i], state[1][i], state[0][i]} ^ {expKey[3][i], expKey[2][i], expKey[1][i], expKey[0][i]};
-                                else {addRoundKeyOut[3][i], addRoundKeyOut[2][i], addRoundKeyOut[1][i], addRoundKeyOut[0][i]} <= {addRoundKeyOut[3][i], addRoundKeyOut[2][i], addRoundKeyOut[1][i], addRoundKeyOut[0][i]};
+                            else begin  // holding the previous round KEYs since sbox_done is not high
+                                {prev_expKey[3][i], prev_expKey[2][i], prev_expKey[1][i], prev_expKey[0][i]} <= {prev_expKey[3][i], prev_expKey[2][i], prev_expKey[1][i], prev_expKey[0][i]};
+                                {addRoundKeyOut[3][i], addRoundKeyOut[2][i], addRoundKeyOut[1][i], addRoundKeyOut[0][i]} <= {addRoundKeyOut[3][i], addRoundKeyOut[2][i], addRoundKeyOut[1][i], addRoundKeyOut[0][i]};
                             end
                         end
                     end
-
-                    round_cntr <= (round_cntr == 4'hA) ? 0 : round_cntr + 1;  // this key size only requires 11 rounds in total
                 end 
+                2'b10: begin  // AES-192
+                    
+                end
+                2'b11: begin  // AES-256
+
+                end
                 default: 
             endcase
         end
--- rtl/type_defs_pkg.sv
+++ rtl/type_defs_pkg.sv
@@ -5,6 +5,7 @@ package type_defs_pkg;
     typedef logic [31:0] word_t;
     typedef logic [3:0][3:0][7:0] state_matrix_t;
     typedef logic [4:0][4:0][63:0] keccak_state_t;
+    typedef logic [3:0][7:0][7:0] expKey_matrix_t;
 
     typedef enum logic [1:0] {
         ABSORB, 
